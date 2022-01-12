8f08b4a864a0115c204217076deeafe903833e19
Akiff Premjee <akiffpremjee@Akiffs-MacBook-Pro.local>
2022-01-11 20:11:55 -0500

    big update

--- src/main.js
+++ src/main.js
@@ -8,6 +8,10 @@ const txnTable = document.querySelector('.txn');
 // wallet addresses for testing - will eventually switch to getting the user's wallet when the connect
 const apWallet = '0xed645e1a76F4BbA630371125e3025304eB8f5462';
 const fundWallet = '0xefB51E9570b4FF2ef30e7Fb6E4B8ac7b19E317b1';
+const convertEth = 10 ** -18;
+let userNFTBuys = {};
+let userNFTSells = {};
+let userAllTxns = {};
 
 /* Authentication code */
 async function login() {
@@ -28,14 +32,12 @@ async function login() {
   return user;
 }
 
-// const user = Moralis.User.current().get('ethAddress');
-// console.log(user);
-
 async function logOut() {
   await Moralis.User.logOut();
   console.log('logged out');
 }
 
+// utility functions
 function handleError(err) {
   console.log('Oh nooo!');
   console.log(err);
@@ -51,81 +53,113 @@ function fixURL(url) {
   return `${url}?format=json`;
 }
 
-async function getNFTTransactions() {
-  const user = await login();
-  const userEthAddress = user.get('ethAddress');
-  console.log(userEthAddress);
+// main functions
+async function getNFTBuys() {
   const options = {
     chain: 'eth',
-    address: userEthAddress, // change this to user's wallet
+    address: fundWallet, // change this to user's wallet
+    direction: 'to',
   };
   const response = await Moralis.Web3API.account.getNFTTransfers(options);
   const data = response.result;
-  console.log(data);
+  userNFTBuys = data;
+  // console.log(data);
+  // data.forEach((txn) => {
+  //   const html = `
+  //       <tr>
+  //         <td>${txn.from_address}</td>
+  //         <td>${txn.to_address}</td>
+  //         <td>${txn.token_address}</td>
+  //         <td>${txn.token_id}</td>
+  //         <td>${txn.contract_type}</td>
+  //         <td>${txn.transaction_hash}</td>
+  //         <td>${txn.transaction_type}</td>
+  //         <td>${txn.value}</td>
+  //       </tr>
+  //       `;
+  //   txnTable.insertAdjacentHTML('afterBegin', html);
+  // });
+}
 
-  data.forEach((txn) => {
+async function getNFTSells() {
+  const options = {
+    chain: 'eth',
+    address: fundWallet, // change this to user's wallet
+    direction: 'from',
+  };
+  const response = await Moralis.Web3API.account.getNFTTransfers(options);
+  const data = response.result;
+  userNFTSells = data;
+}
+
+async function buildData() {
+  // set up buy and sell objects
+  await getNFTBuys().catch(handleError);
+  await getNFTSells().catch(handleError);
+  // fill in useralltxns with data from buy
+  userAllTxns = userNFTBuys.map((input) => ({
+    token_address: input.token_address,
+    token_id: input.token_id,
+    image: 'later',
+    state: null,
+    buy_price: input.value * convertEth,
+    sell_price: null,
+    net_profit: null,
+    floor_price: 'later',
+    traits: 'later',
+    floor_by_trait: 'later',
+  }));
+  // check for sells and populate data accordingly
+  userAllTxns.forEach((item) => {
+    if (
+      userNFTSells.some(
+        (txn) =>
+          txn.token_address === item.token_address &&
+          txn.token_id === item.token_id
+      )
+    ) {
+      item.state = 'sold';
+      item.sell_price =
+        userNFTSells.filter(
+          (txn) => txn.token_address === item.token_address
+        )[0].value * convertEth;
+      item.net_profit = item.sell_price - item.buy_price;
+    } else {
+      item.state = 'hold';
+    }
+  });
+}
+
+// build HTML functions
+async function perNFTTable() {
+  await buildData();
+  console.log(userAllTxns);
+  userAllTxns.forEach((txn) => {
     const html = `
-        <tr>    
-          <td>${txn.from_address}</td>
-          <td>${txn.to_address}</td>
+        <tr>
           <td>${txn.token_address}</td>
           <td>${txn.token_id}</td>
-          <td>${txn.contract_type}</td>
-          <td>${txn.transaction_hash}</td>
-          <td>${txn.transaction_type}</td>
-          <td>${txn.value}</td>
+          <td>${txn.image}</td>
+          <td>${txn.state}</td>
+          <td>${txn.buy_price}</td>
+          <td>${txn.sell_price}</td>
+          <td>${txn.net_profit}</td>
+          <td>${txn.floor_price}</td>
+          <td>${txn.traits}</td>
+          <td>${txn.floor_by_trait}</td>
         </tr>
         `;
     txnTable.insertAdjacentHTML('afterBegin', html);
   });
 }
 
-// archive functions
-async function getNFT() {
-  const options = {
-    chain: 'eth',
-    address: fundWallet, // change this to user's wallet
-  };
-  const response = await Moralis.Web3API.account.getNFTs(options);
-  console.log(response);
-  const data = response.result;
-  console.log(data);
-
-  data.forEach((nft) => {
-    const collectionName = nft.name;
-    const uri = nft.token_uri;
-    fetch(uri)
-      .then((res) => res.json())
-      .then((dt) => {
-        console.log(dt);
-        const imgURL = fixURL(dt.image);
-        const html = `
-        <tr>    
-        <td>${collectionName}</td>
-          <td>${dt.name}</td>
-          <td><img src="${dt.image}" alt="NFT Image" width="100" height="100"></td>
-        </tr>
-        `;
-        console.log(html);
-        txnTable.insertAdjacentHTML('afterBegin', html);
-      })
-      .catch(handleError);
-  });
-}
-
-async function getAllTransactions() {
-  const options = {
-    chain: 'eth',
-    address: fundWallet, // change this to user's wallet
-  };
-  const response = await Moralis.Web3API.account.getTransactions(options);
-  console.log(response);
-}
-
 document.getElementById('btn-login').onclick = login;
 document.getElementById('btn-logout').onclick = logOut;
-document.getElementById('btn-txn').onclick = getNFTTransactions;
+// document.getElementById('btn-txn').onclick = getNFTBuys;
 // call functions
-getNFTTransactions().catch(handleError);
+// getNFTBuys().catch(handleError);
+// getNFTSells().catch(handleError);
+// buildData();
+perNFTTable();
 
 // run tailwind with: npx tailwindcss -i ./src/input.css -o ./dist/output.css --watch
